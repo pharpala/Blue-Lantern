3797a95fef0749d5691a165d328d243fdb203469
Pranay Harpalani <pranayinspain@gmail.com>
2024-06-02 21:19:05 -0400

    added all home components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -36,12 +36,13 @@ const Home = () => {
           displayCoin.slice(0, 10).map((item, index) => (
             <div className="tableLayout" key={index}>
               <p> {item.market_cap_rank}</p>
-              <div>
+              <div className='price'>
                 <img src={item.image} />
+                <p></p>
                 <p>{item.name +' - '+ item.symbol}</p>
               </div>
               <p> {currency.symbol} {item.current_price.toLocaleString()} </p>
-              <p> {Math.floor(item.price_change_percentage_24h * 100)/100}%</p>
+              <p className= {item.price_change_percentage_24h >0 ? "green": "red"}> {Math.floor(item.price_change_percentage_24h * 100)/100}%</p>
               <p className='marketCap'> {currency.symbol}{item.market_cap.toLocaleString()} </p>
             </div>
           ))
