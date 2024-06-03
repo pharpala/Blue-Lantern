d3798f0d1ff5138f1280a74d3d37784bb8fda7e4
Pranay Harpalani <pranayinspain@gmail.com>
2024-06-03 18:49:59 -0400

    added favicon

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -7,7 +7,7 @@ import LineChart from '../../components/LineChart/LineChart';
 const Coin = () => {
 
   const {coinId} = useParams();
-  const [coinData, setCoinData] = useState();
+  const [coinData, setCoinData] = useState(null);
   const [historicalData, setHistoricalData] = useState();
   const {currency} = useContext(CoinContext);
 
@@ -48,49 +48,47 @@ const Coin = () => {
   }, [currency])
 
 
-if(coinData, historicalData) {
+  if (!coinData || !historicalData) {
     return (
-      <div className="coin">
-        <div className="coinName">
-          <img src={coinData.image.large} alt="" />
-          <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
-        </div>
-        <div className="coinChart">
-          <LineChart historicalData = {historicalData}/>
-        </div>
-
-        <div className="coinInfo">
-        <ul>
-            <li> Crypto Market Rank </li>
-            <li> {coinData.market_cap_rank}</li>
-          </ul>
-          <ul>
-            <li> Current Price </li>
-            <li> {currency.symbol} {coinData.market_data.current_price[currency.name].toLocaleString()}</li>
-          </ul>
-          <ul>
-            <li> Market Cap </li>
-            <li> {currency.symbol} {coinData.market_data.market_cap[currency.name].toLocaleString()}</li>
-          </ul>
-          <ul>
-            <li> 24 Hour High </li>
-            <li> {currency.symbol} {coinData.market_data.high_24h[currency.name].toLocaleString()}</li>
-          </ul>
-          <ul>
-            <li> 24 Hour Low </li>
-            <li> {currency.symbol} {coinData.market_data.low_24h[currency.name].toLocaleString()}</li>
-          </ul>
-        </div>
-
+      <div className="spinner">
+        <div className="spin"></div>
       </div>
     )
-  } else {
-      return (
-        <div className="spinner">
-          <div className="spin"></div>
-        </div>
-      )
-    }
+  }
+
+  return (
+    <div className="coin">
+      <div className="coinName">
+        <img src={coinData.image?.large} alt="" />
+        <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+      </div>
+      <div className="coinChart">
+        <LineChart historicalData={historicalData} />
+      </div>
+      <div className="coinInfo">
+        <ul>
+          <li> Crypto Market Rank </li>
+          <li> {coinData.market_cap_rank}</li>
+        </ul>
+        <ul>
+          <li> Current Price </li>
+          <li> {currency.symbol} {coinData.market_data?.current_price[currency.name]?.toLocaleString()}</li>
+        </ul>
+        <ul>
+          <li> Market Cap </li>
+          <li> {currency.symbol} {coinData.market_data?.market_cap[currency.name]?.toLocaleString()}</li>
+        </ul>
+        <ul>
+          <li> 24 Hour High </li>
+          <li> {currency.symbol} {coinData.market_data?.high_24h[currency.name]?.toLocaleString()}</li>
+        </ul>
+        <ul>
+          <li> 24 Hour Low </li>
+          <li> {currency.symbol} {coinData.market_data?.low_24h[currency.name]?.toLocaleString()}</li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Coin
